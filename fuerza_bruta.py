6ff31a772172a3916f978bf8f61c6bc38d9b8912
Katheryn-Caceres-S <kcs.ingcivil@gmail.com>
2025-05-21 01:59:35 +0000

    guardando

--- fuerza_bruta.py
+++ fuerza_bruta.py
@@ -1,1 +1,28 @@
-import string
+from string import ascii_lowercase
+import getpass
+
+intentos = 0
+adivinada = ""
+
+clave = input("Ingrese la contraseña: ").lower()
+
+for letra_clave in clave:
+    for letra in ascii_lowercase:
+        intentos += 1
+        if letra == letra_clave:
+            adivinada += letra
+            break
+
+print(f"La clave fue forzada en {intentos} intentos.")
+
+
+#while clave != "gato":
+ #   print("Clave incorrecta. Inténtalo de nuevo.")
+  #  clave = getpass.getpass("clave: ")
+
+# print("Clave correcta")
+
+
+ #   caracter = 'b'
+  #  if caracter in string.ascii_lowercase:
+   #     print(f"{caracter} es una letra minúscula")
--- primeros_auxilios.py
+++ primeros_auxilios.py
@@ -2,34 +2,36 @@ print("Indice de primeros auxilios")
 ## input(print("El paciente responde a estimulos??"))
 
 while True:
-    estimulo = input("El paciente reponde a estimulos?? (si o no: ) ").lower()
+    estimulo = input("El paciente reponde a estimulos?? (si o no): ").lower()
 
     if estimulo == "si":
-        print("Traslade al paciente al hospital más cercano")
+        print("Valora la necesidad de trasladar al paciente al hospital más cercano")
         print("Evaluación terminada")
         break
 
     elif estimulo == "no":
         print("Revisar la vía Aerea")
-        respira = input("¿Esta respirando? (si o no: ) ").lower()
+        respira = input("¿Esta respirando? (si o no): ").lower()
 
         if respira == "si":
-            print("Permite posición suficiente para ventilación")
+            print("Permitele al paciente posición de suficiente ventilación")
             print("Evaluación terminada")
             break
 
         elif respira == "no":
-            print("Administra 5 ventilaciones y llama a la abulancia")  
+            print("Administra 5 ventilaciones y llama a la ambulancia")  
 
             while True:
-                vida = input("El paciente presenta signos de vida?? (si o no) : ") 
+                vida = input("El paciente presenta signos de vida?? (si o no): ") 
 
                 if vida == "no":
-                    print("Administra compresiones toracicas hasta que llgue la ambulancia")
+                    print("Administra compresiones toracicas hasta que llegue la ambulancia")
                 elif vida == "si":
                     print("Reevaluar a la espera de la ambulancia")
-                
-                ambulancia = input("llego la ambulancia?? (si o no): ").lower()
+                else:
+                    print("Intenta otra vez, solo responder SI o NO") 
+
+                ambulancia = input("¿Llego la ambulancia?? (si o no): ").lower()
 
                 if ambulancia == "si":
                     print("Evaluación terminada")
